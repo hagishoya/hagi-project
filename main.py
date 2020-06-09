563ea9463e2053baf6e3377e276621da64426f07
hagiw shoya <waiton0916@icloud.com>
2020-06-09 12:29:08 +0900

    challenge

--- main.py
+++ main.py
@@ -24,19 +24,19 @@ def callback():
         abort(400)
     return "OK"
 
-#@handler.add(MessageEvent,message=TextMessage)
-#def handle_message(event):
-#    line_bot_api.reply_message(event.reply_token,
-#                               [
-#                                   TextSendMessage(text=event.message.text),
-#                                   TextSendMessage(text="おつかれさまです。"),
-#                                ]
-#                               )
+@handler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(event.reply_token,
+                               [
+                                   TextSendMessage(text=event.message.text),
+                                   TextSendMessage(text="おつかれさまです。"),
+                               ]
+                               )
 @handler.add(MessageEvent,message=ImageMessage)
 def make_picture():
     messages = ImageSendMessage(
-        original_content_url="https://dol.ismcdn.jp/mwimgs/7/1/670m/img_71c53c1d81500a1cf73a4f543e72413f27838.png",  # JPEG 最大画像サイズ：240×240 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
-        preview_image_url="https://dol.ismcdn.jp/mwimgs/7/1/670m/img_71c53c1d81500a1cf73a4f543e72413f27838.png"  # JPEG 最大画像サイズ：1024×1024 最大ファイルサイズ：1MB(注意:仕様が変わっていた)
+        original_content_url="https://dol.ismcdn.jp/mwimgs/7/1/670m/img_71c53c1d81500a1cf73a4f543e72413f27838.png",
+        preview_image_url="https://dol.ismcdn.jp/mwimgs/7/1/670m/img_71c53c1d81500a1cf73a4f543e72413f27838.png"
     )
     return messages
 
