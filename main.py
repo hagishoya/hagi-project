08ca386e23659d46f4436acc99418799f8cb1d80
hagiw shoya <waiton0916@icloud.com>
2020-06-09 09:58:13 +0900

    hello

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def callback():
 
 @handler.add(MessageEvent,message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text,"hello"))
 
 if __name__=="__main__":
     port=int(os.getenv("PORT",5000))
