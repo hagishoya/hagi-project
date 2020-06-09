59e794f8228e992dff8a1ce3e7aa73493bd07a7c
hagiw shoya <waiton0916@icloud.com>
2020-06-09 10:02:16 +0900

    double

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def callback():
 
 @handler.add(MessageEvent,message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text,"hello"))
-
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
 if __name__=="__main__":
     port=int(os.getenv("PORT",5000))
     app.run(host="0.0.0.0",port=port)
