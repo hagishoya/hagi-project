d3959b5bcd2cafb3e464e97cd8642bd1af4269ee
hagiw shoya <waiton0916@icloud.com>
2020-06-16 10:08:39 +0900

    image_save

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def handle_image(event):
 def handle_image_message(event):
     message_content = line_bot_api.get_message_content(event.message.id)
 
-    with open("static/" + event.message.id + "jpg" , "wb") as f:
+    with open("static/" + event.message.id + ".jpg", "wb") as f:
         f.write(message_content.content)
         line_bot_api.reply_message(event.reply_token,ImageSendMessage(
             original_content_url="https://project-hagi.herokuapp.com" + "/static/" + event.message.id + ".jpg",
