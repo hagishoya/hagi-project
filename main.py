c843f6c8203e7f02bd2fccad26e961ec0a93befa
hagiw shoya <waiton0916@icloud.com>
2020-06-16 13:26:56 +0900

    image_save

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask,request,abort
 from linebot import LineBotApi,WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent,TextMessage,TextSendMessage,ImageSendMessage,ImageMessage
-import os,pathlib
+import os
 
 app=Flask(__name__)
 
