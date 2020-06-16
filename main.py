a5d4c533b89f1d0eb5f65927aa452db9799273a3
hagiw shoya <waiton0916@icloud.com>
2020-06-16 10:20:10 +0900

    image_save

--- main.py
+++ main.py
@@ -46,17 +46,17 @@ def handle_image(event):
     )
 
 
-#@handler.add(MessageEvent,message=ImageMessage)
-#def handle_image_message(event):
-#    message_content = line_bot_api.get_message_content(event.message.id)
-#
-#    with open("static/" + event.message.id + ".jpg", "wb") as f:
-#        f.write(message_content.content)
-#        line_bot_api.reply_message(event.reply_token,ImageSendMessage(
-#            original_content_url="https://project-hagi.herokuapp.com" + "/static/" + event.message.id + ".jpg",
-#            preview_image_url = "https://project-hagi.herokuapp.com" + "/static/" + event.message.id + ".jpg",
-#        )
-#                                   )
+@handler.add(MessageEvent,message=ImageMessage)
+def handle_image_message(event):
+    message_content = line_bot_api.get_message_content(event.message.id)
+
+    with open("static/" + event.message.id + ".jpg", "wb") as f:
+        f.write(message_content.content)
+        line_bot_api.reply_message(event.reply_token,ImageSendMessage(
+            original_content_url="https://project-hagi.herokuapp.com" + "/static/" + event.message.id + ".jpg",
+            preview_image_url = "https://project-hagi.herokuapp.com" + "/static/" + event.message.id + ".jpg",
+        )
+                                   )
 
 
 if __name__=="__main__":
