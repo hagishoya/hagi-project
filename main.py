109af2d9a885e5c9ffd23fc32846337a75875f50
syo <hagi19s022@gmail.com>
2020-06-30 10:48:40 +0900

    third_6_30

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-from flask import Flask,request,abort
-from linebot import LineBotApi,WebhookHandler
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent,TextMessage,TextSendMessage,ImageSendMessage,ImageMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, ImageMessage
 import os
 
 app=Flask(__name__)
 
-YOUR_CHANNEL_ACCESS_TOKEN="rl1NmaTQR7jCWwiRmTGxq/6qVAB08MXr97h0a3FiTp4yo/yyPIdBfDI2aDUEuoZOGDnIS5ujoAtsNG7eEW5V4QDgIQSuL892yo0vLbELt9OSliCUu0iJG4dqzRQwOtzxImdNfMAO+D8JWcxZS8fntgdB04t89/1O/w1cDnyilFU="
-YOUR_CHANNEL_SECRET="9f66f5b734e9db071bf0a5c535429bf4"
-line_bot_api=LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
-handler=WebhookHandler(YOUR_CHANNEL_SECRET)
+YOUR_CHANNEL_ACCESS_TOKEN = "rl1NmaTQR7jCWwiRmTGxq/6qVAB08MXr97h0a3FiTp4yo/yyPIdBfDI2aDUEuoZOGDnIS5ujoAtsNG7eEW5V4QDgIQSuL892yo0vLbELt9OSliCUu0iJG4dqzRQwOtzxImdNfMAO+D8JWcxZS8fntgdB04t89/1O/w1cDnyilFU="
+YOUR_CHANNEL_SECRET = "9f66f5b734e9db071bf0a5c535429bf4"
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 
 FQDN = "https://project-hagi.herokuapp.com"
 
 
-@app.route("/callback",methods=["POST"])
+@app.route("/callback", methods=["POST"])
 def callback():
     signature=request.headers["X-Line-Signature"]
 
@@ -29,15 +29,15 @@ def callback():
     return "OK"
 
 
-@handler.add(MessageEvent,message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,
-                               [
-                                   TextSendMessage(text=event.message.text),
-                                   TextSendMessage(text="おつかれさまです。"),
-                                   TextSendMessage(text=event.message.id),
-                               ]
-                               )
+#@handler.add(MessageEvent,message=TextMessage)
+#def handle_message(event):
+#    line_bot_api.reply_message(event.reply_token,
+#                               [
+#                                   TextSendMessage(text=event.message.text),
+#                                   TextSendMessage(text="おつかれさまです。"),
+#                                   TextSendMessage(text=event.message.id),
+#                               ]
+#                               )
 
 
 #@handler.add(MessageEvent,message=ImageMessage)
@@ -59,13 +59,13 @@ def handle_image_message(event):
         f.write(message_content.content)
         line_bot_api.reply_message(
             event.reply_token,ImageSendMessage(
-                original_content_url=FQDN + "/static/"+ event.message.id + ".jpg",
-                preview_image_url=FQDN +"/static/"+ event.message.id + ".jpg",
+                original_content_url=FQDN + "/static/" + event.message.id + ".jpg",
+                preview_image_url=FQDN + "/static/" + event.message.id + ".jpg",
             )
         )
     print(event.message.id)
 
 
 if __name__=="__main__":
-    port=int(os.getenv("PORT",5000))
+    port = int(os.getenv("PORT",5000))
     app.run(host="0.0.0.0",port=port)
