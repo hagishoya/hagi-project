0272cc975c17bdb76a8542224ab80bd90a273b1a
syo <hagi19s022@gmail.com>
2020-06-30 12:10:05 +0900

    seven_6_30

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, ImageMessage
 import os
-import cv2
 
 app=Flask(__name__)
 
@@ -18,9 +17,9 @@ FQDN = "https://project-hagi.herokuapp.com"
 # 分類器ディレクトリ
 cascade_path = "./models/haarcascade_frontalface_default.xml"
 # 使用ファイルと入出力ディレクトリ
-#image_file = event.message.id + ".jpg"
-#image_path = FQDN + "/static/" + image_file
-#output_path = FQDN + "/static/" + image_file
+image_file = event.message.id + ".jpg"
+image_path = "/static/" + image_file
+output_path = FQDN + "/static/" + image_file
 
 
 
@@ -66,12 +65,49 @@ def handle_message(event):
 
 
 @handler.add(MessageEvent,message=ImageMessage)
+def change_image():
+    # ファイル読み込み
+    image = cv2.imread(image_path)
+
+    # グレースケール変換
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # カスケード分類器の特徴量を取得する
+    cascade = cv2.CascadeClassifier(cascade_path)
+
+    # 物体認識（顔認識）の実行
+    # image – CV_8U 型の行列．ここに格納されている画像中から物体が検出されます
+    # objects – 矩形を要素とするベクトル．それぞれの矩形は，検出した物体を含みます
+    # scaleFactor – 各画像スケールにおける縮小量を表します
+    # minNeighbors – 物体候補となる矩形は，最低でもこの数だけの近傍矩形を含む必要があります
+    # flags – このパラメータは，新しいカスケードでは利用されません．古いカスケードに対しては，cvHaarDetectObjects 関数の場合と同じ意味を持ちます
+    # minSize – 物体が取り得る最小サイズ．これよりも小さい物体は無視されます
+    facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30))
+
+    # print(facerect)
+    color = (255, 255, 255)  # 白
+
+    # 検出した場合
+    if len(facerect) > 0:
+
+        # 検出した顔を囲む矩形の作成
+        for rect in facerect:
+            cv2.rectangle(image, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
+
+        # 認識結果の保存
+        cv2.imwrite(output_path, image)
+
+
+
+
+
 def handle_image_message(event):
     message_content = line_bot_api.get_message_content(event.message.id)
     if not os.path.exists('static'):
         os.mkdir('static/')
     with open("static/" + event.message.id + ".jpg", "wb") as f:
         f.write(message_content.content)
+        change_image()
         line_bot_api.reply_message(
             event.reply_token,ImageSendMessage(
                 original_content_url=FQDN + "/static/" + event.message.id + ".jpg",
