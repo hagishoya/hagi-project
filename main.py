f2db2746e0ce7a19b8fc1b78050f4ab056508c6f
syo <hagi19s022@gmail.com>
2020-06-30 10:53:39 +0900

    forth_6_30

--- main.py
+++ main.py
@@ -29,15 +29,15 @@ def callback():
     return "OK"
 
 
-#@handler.add(MessageEvent,message=TextMessage)
-#def handle_message(event):
-#    line_bot_api.reply_message(event.reply_token,
-#                               [
-#                                   TextSendMessage(text=event.message.text),
-#                                   TextSendMessage(text="おつかれさまです。"),
-#                                   TextSendMessage(text=event.message.id),
-#                               ]
-#                               )
+@handler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(event.reply_token,
+                               [
+                                   TextSendMessage(text=event.message.text),
+                                   TextSendMessage(text="おつかれさまです。"),
+                                   TextSendMessage(text=event.message.id),
+                               ]
+                               )
 
 
 #@handler.add(MessageEvent,message=ImageMessage)
