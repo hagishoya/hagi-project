bb9b328feea04e4100071e6312ca931ea15badb6
syo <hagi19s022@gmail.com>
2020-07-03 10:40:27 +0900

    first_7_3

--- main.py
+++ main.py
@@ -61,21 +61,21 @@ def handle_message(event):
 
 
 @handler.add(MessageEvent,message=ImageMessage)
-def change_image(event):
+#def change_image(event):
     # 分類器ディレクトリ
-    cascade_path = "./models/haarcascade_frontalface_default.xml"
+#    cascade_path = "./models/haarcascade_frontalface_default.xml"
     # 使用ファイルと入出力ディレクトリ
-    image_file = event.message.id + ".jpg"
-    image_path = "/static/" + image_file
-    output_path = FQDN + "/static/" + image_file
+#    image_file = event.message.id + ".jpg"
+#    image_path = "/static/" + image_file
+#    output_path = FQDN + "/static/" + image_file
     # ファイル読み込み
-    image = cv2.imread(image_path)
+#    image = cv2.imread(image_path)
 
     # グレースケール変換
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # カスケード分類器の特徴量を取得する
-    cascade = cv2.CascadeClassifier(cascade_path)
+ #   cascade = cv2.CascadeClassifier(cascade_path)
 
     # 物体認識（顔認識）の実行
     # image – CV_8U 型の行列．ここに格納されている画像中から物体が検出されます
@@ -84,20 +84,20 @@ def change_image(event):
     # minNeighbors – 物体候補となる矩形は，最低でもこの数だけの近傍矩形を含む必要があります
     # flags – このパラメータは，新しいカスケードでは利用されません．古いカスケードに対しては，cvHaarDetectObjects 関数の場合と同じ意味を持ちます
     # minSize – 物体が取り得る最小サイズ．これよりも小さい物体は無視されます
-    facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30))
+#    facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30))
 
     # print(facerect)
-    color = (255, 255, 255)  # 白
+#    color = (255, 255, 255)  # 白
 
     # 検出した場合
-    if len(facerect) > 0:
+ #   if len(facerect) > 0:
 
         # 検出した顔を囲む矩形の作成
-        for rect in facerect:
-            cv2.rectangle(image, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
+#        for rect in facerect:
+#            cv2.rectangle(image, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
 
         # 認識結果の保存
-        cv2.imwrite(output_path, image)
+#        cv2.imwrite(output_path, image)
 
 
 
@@ -109,7 +109,7 @@ def handle_image_message(event):
         os.mkdir('static/')
     with open("static/" + event.message.id + ".jpg", "wb") as f:
         f.write(message_content.content)
-        change_image(event)
+        #change_image(event)
         line_bot_api.reply_message(
             event.reply_token,ImageSendMessage(
                 original_content_url=FQDN + "/static/" + event.message.id + ".jpg",
