51f4a587e07b56e7866f347f5c69b3bf5dfa4da6
syo <hagi19s022@gmail.com>
2020-07-14 11:16:23 +0900

    imread変更

--- main.py
+++ main.py
@@ -57,10 +57,11 @@ def change_image(event):
     cascade_path = "haarcascade_frontalface_default.xml"
 # 使用ファイルと入出力ディレクトリ
     image_file = event.message.id + ".jpg"
-    image_path = FQDN + "/static/" + image_file
+    image_path = "static/" + image_file
     output_path = FQDN + "/static/" + image_file
 # ファイル読み込み
     image = cv2.imread(image_path)
+    print("image: {}".format(type(image)))
 
 # グレースケール変換
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
