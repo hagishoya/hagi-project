4e606c4bcf0619b1c2e99e099afac29fb06b02eb
syo <hagi19s022@gmail.com>
2020-07-14 11:53:11 +0900

    imread変更

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, ImageMessage
 import os
-#import cv2
+import cv2
 
 
 app = Flask(__name__)
@@ -52,8 +52,6 @@ def handle_message(event):
 #    )
 
 @handler.add(MessageEvent, message=ImageMessage)
-
-
 def handle_image_message(event):
     print("メッセージID")
     print(event.message.id)
@@ -62,7 +60,7 @@ def handle_image_message(event):
         os.mkdir('static/')
     with open("static/" + event.message.id + ".jpg", "wb") as f:
         f.write(message_content.content)
-        #change_image(event)
+        change_image(event)
         line_bot_api.reply_message(
             event.reply_token, ImageSendMessage(
                 original_content_url=FQDN + "/static/" + event.message.id + ".jpg",
@@ -70,7 +68,47 @@ def handle_image_message(event):
             )
         )
 
-    print(message_content.content)
+def change_image(event):
+    cascade_path = "haarcascade_frontalface_default.xml"
+
+    image_file = event.message.id + ".jpg"
+    print("イメージファイル: {}".format(image_file))
+    image_path = "static/" + image_file
+    print("イメージパス: {}".format(image_path))
+    output_path = FQDN + "/static/" + image_file
+    print("アウトプットパス: {}".format(output_path))
+
+    # ファイル読み込み
+    image = cv2.imread(image_path)
+
+    # グレースケール変換
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # カスケード分類器の特徴量を取得する
+    cascade = cv2.CascadeClassifier(cascade_path)
+
+    # 物体認識（顔認識）の実行
+    # image – CV_8U 型の行列．ここに格納されている画像中から物体が検出されます
+    # objects – 矩形を要素とするベクトル．それぞれの矩形は，検出した物体を含みます
+    # scaleFactor – 各画像スケールにおける縮小量を表します
+    # minNeighbors – 物体候補となる矩形は，最低でもこの数だけの近傍矩形を含む必要があります
+    # flags – このパラメータは，新しいカスケードでは利用されません．古いカスケードに対しては，cvHaarDetectObjects 関数の場合と同じ意味を持ちます
+    # minSize – 物体が取り得る最小サイズ．これよりも小さい物体は無視されます
+    facerect = cascade.detectMultiScale(image_gray, scaleFactor=1.1, minNeighbors=2, minSize=(30, 30))
+
+    # print(facerect)
+    color = (255, 255, 255)  # 白
+
+    # 検出した場合
+    if len(facerect) > 0:
+
+        # 検出した顔を囲む矩形の作成
+        for rect in facerect:
+            cv2.rectangle(image, tuple(rect[0:2]), tuple(rect[0:2] + rect[2:4]), color, thickness=2)
+
+        # 認識結果の保存
+        cv2.imwrite(output_path, image)
+
 
 
 if __name__ == "__main__":
