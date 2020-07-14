0aa23aa50b8bf857cc9e96f7044acebcfe1d7541
syo <hagi19s022@gmail.com>
2020-07-14 12:48:01 +0900

    imread変更

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ def handle_image_message(event):
         change_image(event)
         line_bot_api.reply_message(
             event.reply_token, ImageSendMessage(
-                original_content_url=FQDN + "/static/" + event.message.id + ".jpg",
-                preview_image_url=FQDN + "/static/" + event.message.id + ".jpg",
+                original_content_url=FQDN + "/static/" + event.message.id + "_face.jpg",
+                preview_image_url=FQDN + "/static/" + event.message.id + "_face.jpg",
             )
         )
 
