480f866788fedd1a224241840552e851ec76ff2a
hagishoya <hagi19s022@gmail.com>
2020-10-20 01:36:29 +0000

    .get

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ path_w1 = 'saveid.txt'
 path_w2 = 'savereply.txt'
 app = Flask(__name__)
 
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ['YOUR_CHANNEL_ACCESS_TOKEN']
-YOUR_CHANNEL_SECRET = os.environ['YOUR_CHANNEL_SECRET']
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get['YOUR_CHANNEL_ACCESS_TOKEN']
+YOUR_CHANNEL_SECRET = os.environ.get['YOUR_CHANNEL_SECRET']
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-FQDN = os.environ['FQDN']
+FQDN = os.environ.get['FQDN']
 
 
 @app.route("/callback", methods=["POST"])
