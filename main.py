265ae5b5d1b29efaf42ab4935fa0c9cf89d1c672
hagishoya <hagi19s022@gmail.com>
2020-10-20 00:41:19 +0000

    enviroment va

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ path_w1 = 'saveid.txt'
 path_w2 = 'savereply.txt'
 app = Flask(__name__)
 
-YOUR_CHANNEL_ACCESS_TOKEN = ""
-YOUR_CHANNEL_SECRET = ""
+YOUR_CHANNEL_ACCESS_TOKEN = YOUR_CHANNEL_ACCESS_TOKEN
+YOUR_CHANNEL_SECRET = YOUR_CHANNEL_SECRET
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-FQDN = ""
+FQDN = FQDN
 
 
 @app.route("/callback", methods=["POST"])
@@ -97,8 +97,7 @@ def flex(event):
     if event.reply_token == "ffffffffffffffffffffffffffffffff":
         return
         
-    message_id = ''
-    line_bot_api.push_message(message_id, messages=messages)
+    line_bot_api.push_message(reply_work, messages=messages)
 
 
 def handle_textmessage(event):
